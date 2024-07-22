8997ca66088b6f70f66b009ec57173319764d80f
吳元皓 <hpware3@gmail.com>
2024-07-22 17:39:06 +0800

    large fix

--- components/lang-loading.js
+++ components/lang-loading.js
@@ -1,12 +1,1 @@
-const lang = new URL(location.href).searchParams.get('lang');
-if (lang === 'zh') {
-    document.getElementById("en-loading").style.display = "none";
-    document.getElementById("zh-loading").style.display = "block";
-}
-if (lang === 'en') {
-    document.getElementById("en-loading").style.display = "block";
-    document.getElementById("zh-loading").style.display = "none";
-}
-else {
-    window.location.href = `${window.location.href}?lang=en`;
-}
+ 
