b9d5d023508599f932718a6fb978666a9f1928d2
吳元皓 <hpware3@gmail.com>
2024-07-22 17:29:18 +0800

    add mobile-style.css

--- components/lang-loading.js
+++ components/lang-loading.js
@@ -1,7 +1,12 @@
-if (navigator.language.includes("zh")) {
-  document.getElementById("en-loading").style.display = "none";
-  document.getElementById("zh-loading").style.display = "block";
-} else {
-  document.getElementById("en-loading").style.display = "block";
-  document.getElementById("zh-loading").style.display = "none";
+const lang = new URL(location.href).searchParams.get('lang');
+if (lang === 'zh') {
+    document.getElementById("en-loading").style.display = "none";
+    document.getElementById("zh-loading").style.display = "block";
+}
+if (lang === 'en') {
+    document.getElementById("en-loading").style.display = "block";
+    document.getElementById("zh-loading").style.display = "none";
+}
+else {
+    window.location.href = `${window.location.href}?lang=en`;
 }
