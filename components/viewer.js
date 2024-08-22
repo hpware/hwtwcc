ee4963abcc5916f2a76d44f5040f82a6b82a9cfa
吳元皓 <157942818+hpware@users.noreply.github.com>
2024-08-22 21:59:26 +0800

    Update viewer.js

--- components/viewer.js
+++ components/viewer.js
@@ -3,7 +3,6 @@ function abtestjs() {
     return Math.random() < 0.5 ? "A" : "B";
 }
 const abtestvar = abtestjs();
-console.log(`${abtestvar}`);
 document.addEventListener('DOMContentLoaded', function() {
     const urlParam = new URL(window.location.href).searchParams.get("i");
     const dirParam = new URL(window.location.href).searchParams.get("d");
@@ -13,34 +12,50 @@ document.addEventListener('DOMContentLoaded', function() {
     if (!optParam) {
         var optSize = "90%";
     }
-    if (optParam) {
+    else if (optParam) {
         var optSize = optParam + "%";
     }
     if (!formatParam) {
         var format = "JPG";
     }
-    if (formatParam) {
+    else if (formatParam) {
         var format = formatParam;
     }
-    if (formatParam == "ip") {
+    else if (formatParam == "ip") {
         var format = "heic";
     }
-    if (formatParam == "IP") {
+    else if (formatParam == "IP") {
         var format = "HEIC";
     }
-    
+
     if (urlParam) {
         if (dirParam) {
             imageViewer.src = "https://ghfiles.yuanh.xyz/images/photolib/" + dirParam + "/" +urlParam + "." + format;
-            download.href = "https://php.hwtw.cc/github-image-download.php?f=photolib/" + dirParam + "/" +urlParam + "." + format;
             imageViewer.alt = "Image from" + dirParam;
             imageViewer.style.width = optSize;
+            if (abtestvar === "A") {
+                download.href = "https://php.hwtw.cc/github-image-download.php?f=photolib/" + dirParam + "/" +urlParam + "." + format;
+                download.download = 
+            }
+            else if (abtestvar === "B") {
+                download.href = ""
+                download.download = urlParam + "." format;
+            }
+            
+
         }
         else {
-            imageViewer.src = "https://ghfiles.yuanh.xyz/images/photolib/" +urlParam + ".JPG";
-            download.href = "https://php.hwtw.cc/github-image-download.php?f=photolib/" + dirParam + "/" +urlParam + "." + format;
+            imageViewer.src = "https://ghfiles.yuanh.xyz/images/photolib/" +urlParam + "." + format;
             imageViewer.alt = "Image Viewer";
             imageViewer.style.width = optSize;
+            if (abtestvar === "A") {
+                download.href = "https://php.hwtw.cc/github-image-download.php?f=photolib/" + dirParam + "/" +urlParam + "." + format;
+                download.download = ""
+            }
+            else if (abtestvar === "B") {
+                download.href = ""
+                download.download = urlParam + "." format;
+            }
         }
     } else {
         document.getElementById('Display').innerHTML = "No image specified";
