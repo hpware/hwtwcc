936dcefeffa88edd3fde29f8c27e5685912860e2
Howard Wu <hw@yuanhau.com>
2026-04-18 17:34:11 +0800

    Changes including adding PHP support ;)

--- components/viewer.js
+++ components/viewer.js
@@ -1,32 +1,58 @@
 const urlParam = new URL(window.location.href).searchParams.get("i");
 const dirParam = new URL(window.location.href).searchParams.get("d");
 const optParam = new URL(window.location.href).searchParams.get("opt");
-const imageViewer = document.getElementById('imageviewer');
+const imageViewer = document.getElementById("imageviewer");
 const formatParam = new URL(window.location.href).searchParams.get("format");
 // A/B Random Testing
-function abtestjs() {return Math.random() < 0.5 ? "A" : "B";} const abtestvar = abtestjs();
+function abtestjs() {
+  return Math.random() < 0.5 ? "A" : "B";
+}
+const abtestvar = abtestjs();
 // Pull Format
-if (!formatParam) {var format = "JPG";}else if (formatParam) {var format = formatParam;}else if (formatParam == "ip") {var format = "heic";}else if (formatParam == "IP") {var format = "HEIC";}
+if (!formatParam) {
+  var format = "JPG";
+} else if (formatParam) {
+  var format = formatParam;
+} else if (formatParam == "ip") {
+  var format = "heic";
+} else if (formatParam == "IP") {
+  var format = "HEIC";
+}
 // Pull Width
-if (!optParam) {var optSize = "90%";}else if (optParam) {var optSize = optParam + "%";}
+if (!optParam) {
+  var optSize = "90%";
+} else if (optParam) {
+  var optSize = optParam + "%";
+}
 //Dun Dun Dun viewer.js
-document.addEventListener('DOMContentLoaded', function() {
-    if (urlParam) {
-        if (dirParam) {
-            imageViewer.src = "https://ghfiles.yuanh.xyz/images/photolib/" + dirParam + "/" +urlParam + "." + format;
-            imageViewer.alt = "Image from" + dirParam;
-            imageViewer.style.width = optSize;
-            download.href = "https://php.hwtw.cc/github-image-download.php?f=photolib/" + dirParam + "/" +urlParam + "." + format;
-        }
-        else {
-            download.href = "https://php.hwtw.cc/github-image-download.php?f=photolib/" + "/" +urlParam + "." + format;
-            imageViewer.src = "https://ghfiles.yuanh.xyz/images/photolib/" +urlParam + "." + format;
-            imageViewer.alt = "Image Viewer";
-            imageViewer.style.width = optSize;
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  if (urlParam) {
+    if (dirParam) {
+      imageViewer.src =
+        "/exec/image.php?path=/photolib/" +
+        dirParam +
+        "/" +
+        urlParam +
+        "." +
+        format;
+      imageViewer.alt = "Image from" + dirParam;
+      imageViewer.style.width = optSize;
+      download.href =
+        "/exec/image.php?dl=1&path=photolib/" +
+        dirParam +
+        "/" +
+        urlParam +
+        "." +
+        format;
     } else {
-        document.getElementById('Display').innerHTML = "No image specified";
+      download.href =
+        "/exec/image.php?dl=1&path=photolib/" + urlParam + "." + format;
+      imageViewer.src =
+        "/exec/image.php?path=/photolib/" + urlParam + "." + format;
+      imageViewer.alt = "Image Viewer";
+      imageViewer.style.width = optSize;
     }
+  } else {
+    document.getElementById("Display").innerHTML = "No image specified";
+  }
 });
-
-
---
+++ exec/image.php
@@ -0,0 +1,9 @@
+<?php
+$dl = $_GET["dl"];
+$path = $_GET["path"];
+
+$serverPath = "/git_data/ghfiles_new/30s";
+
+die("Work in progress");
+
+?>
